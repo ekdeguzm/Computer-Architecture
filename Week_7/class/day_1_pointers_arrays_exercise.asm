6d8472f1ee977fdc74a20c82b38fdd7af126824a
Emil De Guzman <em.deguzm@gmail.com>
2024-10-07 22:01:09 -0700

    create assignment file

--- Week_7/class/day_1_pointers_arrays_exercise.asm
+++ Week_7/class/day_1_pointers_arrays_exercise.asm
@@ -1,5 +1,10 @@
+# 1. Load address of string, add +1, +2, +3 to address, load byte see what it is.
+	# you add to the 268,500,000 which can give you a or ! or @
+	# it is very useful to do math on the addresses.
+# 2. Load first byte ('a'), add +1, +2, +3, to byte
+	# doing math here say i get 97 "a", then when I add 1, it gives me 98b, 
+	
 	.data
-# string:	.asciiz "abcdefghijklmnop" 
 string:		.asciiz "a!@&T."
 
 # a = 97	(b = 98, # c = 99, ...)
@@ -23,14 +28,22 @@ main:
 	# $a0 = 97
 	lb 	$a0, ($t0)	# load byte
 				# most similar to: load word
+	# move 	$a0, $t0
+				
+	li 	$v0, 1
+	syscall
 	
-	# $a0 = 268,500,992			
-	move 	$a0, $t0	
+	li	$v0, 11
+	syscall
+	
+	subi 	$a0, $a0, 64
 	
 	li 	$v0, 1
 	syscall
 	
 	li	$v0, 11
 	syscall
+
+	
 	
 	jr	$ra
