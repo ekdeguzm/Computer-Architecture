6e26dae420fcb8a6f5bd320d68528dccf4fe269b
Emil De Guzman <em.deguzm@gmail.com>
2024-10-22 16:13:01 -0700

    create lab folder, compelte warm up

---
+++ Week_8/class/day_2_warm_up_pointers_arrays_fit_it_answer.asm
@@ -0,0 +1,32 @@
+# Replace all the numbers greater than 10 with 10.
+#
+# e.g. 5, 7, 17, 12, 2 --> 5, 7, 10, 10, 2
+
+    .data
+array: .word 8, 6, 15, 22, 0, 7
+size:  .word 6
+
+    .text
+    .globl main
+main:
+    lw    $t0, 0
+    lw    $t1, array
+    lw    $t2, size
+    lw    $t3, 10
+
+    LloopBegin:
+        bgt     $t0, $t2, LloopEnd
+        lw      $t4, ($t1)
+        bgt     $t4, $t3, LifGreater
+
+        LifGreater:
+             lw    $t3, ($t1)
+
+        LifEnd:
+
+        addi    $t0, $t0, 1
+        b       LloopBegin
+
+    LloopEnd:
+
+    jr    $ra
---
+++ Week_8/class/day_2_warm_up_pointers_arrays_fix_it.asm
@@ -0,0 +1,53 @@
+# Replace all the numbers greater than 10 with 10.
+#
+# e.g. 5, 7, 17, 12, 2 --> 5, 7, 10, 10, 2
+
+    .data
+array: .word 8, 6, 15, 22, 0, 7
+size:  .word 6
+
+    .text
+    .globl main
+main:
+    li    $t0, 0			# index i	
+    la    $t1, array			# load address into $t1
+    lw    $t2, size			# this is the size of array
+    li    $t3, 10			# the number we want to load into it
+
+
+
+    LloopBegin:
+    
+
+        bge	$t0, $t2, LloopEnd	# is bge the best since 0 -> 5 would check every array
+        
+        # get offset number
+        sll	$t5, $t0, 2
+        
+        # add offset to interate through array 
+       	add 	$t4, $t1, $t5
+        
+        lw      $t6, ($t4)		# t4 contains $t1's number
+       
+        
+        bgt   	$t6, $t3, LifGreater	# if the number in t4 is greater than 10, branch to LifGreater
+
+	b 	LifEnd			# branch to end to increment
+	
+        LifGreater:
+             sw    $t3, ($t4)		# put number 10 into the unit
+
+        LifEnd:	
+    		# print what is it $t4
+      		li	$v0, 1
+      		lw 	$a0, ($t4)	
+    		syscall		
+
+        	addi    $t0, $t0, 1
+        	b       LloopBegin
+        
+        	
+
+    LloopEnd:
+
+    jr    $ra
