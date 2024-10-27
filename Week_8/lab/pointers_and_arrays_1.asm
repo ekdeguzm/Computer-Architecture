899b41bb8a6d65ac872364d5defeca5fc468f5c1
Emil De Guzman <em.deguzm@gmail.com>
2024-10-27 00:03:27 -0700

    begin assignment pointers and arrays

--- Week_8/lab/pointers_and_arrays_1.asm
+++ Week_8/lab/pointers_and_arrays_1.asm
@@ -20,46 +20,35 @@ main:
 	# Setup (don't worry about
 	# this code)
 	#############################
-	addiu	$sp, $sp, -20
+	addiu	        $sp, $sp, -20
 	sw		$ra, 16($sp)
 	
 	#############################
 	# Your code here...
 	#############################
-	
-# Problem Three: Group Set 3:
+	# I changed the code for problem 1 so that it checks that result is within the array bounds before indexing into the array:
 
-# result = 0;
-# for (i = Nelems - 1; i != 0; i--)
-# {
-#     if (arr[i] < 0) 
-#     {
-#         arr[i] = -arr[i]; result++; 
-#     }
-# }
-
-	li		$t7, 0			# this is our index
-	lw		$t0, Nelems		# $t0 is i
-        addi            $t0, $t0, -1		# we want to get one lower that our Nelems to fit index 0-8
-	la		$t1, arr		# load address into $t1
+	# result = 0;
+	# while (result < Nelems && arr[result] != num)
+	# {
+	#     result++;
+	# }
 	
+	li		$t7, 0            # $t7 is result, or index
+	lw		$t0, Nelems
+	la		$t1, arr
+	lw		$t2, num
+
 Lbegin:
-	blt		$t0, $zero, Lend	# if (i < 0) goto Lend;
-	sll		$t2, $t0, 2		# multiply index by 4 and store in $t2
-	add		$t2, $t2, $t1		# add that offset into the original address and store in $t2
-	lw		$t3, 0($t2)		# retrieve arr[i] from $t2
-	bge		$t3, $zero, Ldec	# if (arr[i] >= 0) goto Ldec
+	bge		$t7, $t0, Lend    # if (result >= Nelems) goto Lend;
+	sll		$t3, $t7, 2       # multiply result by 4
+	add		$t3, $t3, $t1     # add to base address of arr
+	lw		$t3, 0($t3)       # retrieve arr[result]
+	beq		$t3, $t2, Lend    # if (arr[result] == num) goto Lend;
+	addi	        $t7, $t7, 1       # result++;
+	b		Lbegin
 
-	sub		$t3, $zero, $t3		# $t3 = 0 - $t3 get the absolute value of negative
-	sw		$t3, 0($t2)		# arr[i] = -arr[i]; make sure that that positive value is now stored in $t2's memory address value
-	addi	        $t7, $t7, 1		# result++;	increment $t7 which is the occurance of negative values
-
-Ldec:
-	addi	        $t0, $t0, -1		# i--;
-	b		Lbegin			# goto Lbegin;
-	
 Lend:
-
 
 	#############################
 	# This code outputs the array.
@@ -72,7 +61,7 @@ Lend:
 	la		$t1, arr
 	lw		$t2, Nelems
 Larr_output_begin:
-	beq 	$t0, $t2, Larr_output_done
+	beq 	        $t0, $t2, Larr_output_done
 	sll		$t3, $t0, 2
 	add		$t4, $t3, $t1
 
@@ -84,7 +73,7 @@ Larr_output_begin:
 	li		$v0, 4
 	syscall
 
-	addi	$t0, $t0, 1
+	addi	        $t0, $t0, 1
 	b		Larr_output_begin
 	
 Larr_output_done:
@@ -101,7 +90,7 @@ Larr_output_done:
 	li		$v0, 4
 	syscall
 
-	move	$a0, $t7
+	move	        $a0, $t7
 	li		$v0, 1
 	syscall
 
@@ -114,5 +103,5 @@ Larr_output_done:
 	# this code)
 	#############################
 	lw		$ra, 16($sp)
-	addiu	$sp, $sp, 20
+	addiu	        $sp, $sp, 20
 	jr		$ra
