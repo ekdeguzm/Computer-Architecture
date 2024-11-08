c7a5823c616fb202a514737747d003a2489a4a08
Emil De Guzman <em.deguzm@gmail.com>
2024-11-07 16:50:09 -0800

    reason why  registers are important

---
+++ Week_11/class/day_2_notes.asm
@@ -0,0 +1,68 @@
+# total = 0				# this is vulnerable, lose everytime we jal
+# for (int i = 0; i < 3: i++) {		# same with i, 
+#					# also we need to save the value of 3. know when to stop our loop!
+#	int number InputConsoleInt(); 	# this is a jal!
+#	total += number;
+# }
+# PrintInteger(total);
+
+# has a loop w/ function call (jal)
+# this means that using $s register is better
+# how many times we go to ram, how many times we load and how many times we store.
+# we want to minimize because that makes things slower
+# storing it in $s makes it that we are within the CPU and within the register
+
+
+# total is in $t0,
+# i is in $t1
+# 3 is in $t2
+...
+...
+
+# if ran a thousand times, 6 thousand memory references, it can be taxing memory wise
+
+LloopBegin:
+
+	sw	#t0,
+	sw	#t1,
+	sw	#t2,
+	
+
+	jal	InputConsoleInt		# danger 
+
+
+	lw	#t0,
+	lw	#t1,
+	lw	#t2,
+	
+LloopEnd:
+
+
+# OK, now here is something different
+# if i do
+# # total is in $s0,
+# i is in $s1
+# 3 is in $s2
+
+
+	sw	#s0,
+	sw	#s1,
+	sw	#s2,
+
+LloopBegin:
+
+	
+
+	jal	InputConsoleInt		# danger 
+
+	
+LloopEnd:
+
+	lw	#s0,
+	lw	#s1,
+	lw	#s2,
+	
+# if ran one thousand or one million times, there are only 6 trips to RAM, fewer total memory references
+# faster!
+
+# good if you have a loop and inside the loop, you are dealing with things that you need to keep safe. 
--- Week_11/class/day_2_warm_up_buffer.asm
+++ Week_11/class/day_2_warm_up_buffer.asm
@@ -2,4 +2,39 @@
 
 # How do you make a 400-byte buffer (empty array) in your global data section?
 
+# You would use .space in the global area. The space will then be within .data. One copy of of this space for the whole 
+# program to share. Shared global space. 
+
+# 	.data
+# buffer:	.space 	400
+#
+# 	.text
+# 	.globl main
+#
+# main:
+#	la	$t0, buffer
+#	addi	$t0, $t0, 100
+#	sw	$t7, ($t0)			$t7 has some integer in it	
+#
+#
+
 # What about in your local data section (stack)?
+
+# Scope of a function. Within a particular function. Requires a bit of math. Consider tax region, and $ra, 16 + 4 + 400 = 420, but has to be 
+# multiple of 8, so 424 bytes. 
+# 
+# This is how you would store information into the stack. 
+# $t0 contains the address 16 above the $sp. 
+# la	$t0, 16($sp)
+# 
+#
+# main:
+#	subi	$sp, $sp, 424
+#	sw	$sp, 420($sp)
+#
+#	...
+#
+#	addi	$sp, $sp, 424
+#	lw	$sp, 420($sp)
+
+# it is possible for a non-leaf function to require a stack frame. Leaf functions can also request space from stack. 
