d049278f9042a7376dbdf75e9e765a57fbbe814b
Emil De Guzman <em.deguzm@gmail.com>
2024-11-12 22:36:04 -0800

    complete class exercise

--- Week_12/class/day_1_class_example_2.asm
+++ Week_12/class/day_1_class_example_2.asm
@@ -9,3 +9,44 @@
 #	PrintString(unil_s_netline);
 #	itoax(answer, endingString);
 #	PrintString(endingString);
+
+	.include "/Users/emildeguzman/Documents/Files/learning/CS270/util.s"
+	.data
+
+startingString:	.asciiz "1b"
+answer: 	.word 	0
+endingString:	.space 	8
+
+	.text
+	.globl main
+	
+main:
+	
+	subi	$sp, $sp, 24
+	sw	$ra, 20($sp)
+
+	la	$a0, answer
+	la	$a1, startingString
+	
+	jal 	axtoi
+	
+	# PrintInteger(answer)
+	lw 	$a0, answer
+	jal 	PrintInteger
+	
+	la	$a0, util_s_newline
+	jal 	PrintString
+	
+	lw	$a0, answer
+	la	$a1, endingString
+	jal	itoax
+	
+	
+	la	$a0, endingString
+	jal	PrintString
+	
+	lw	$ra, 20($sp)		
+	addi	$sp, $sp, 24
+	jr	$ra
+	
+	
